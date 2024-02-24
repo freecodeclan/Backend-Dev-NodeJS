8e2d9eb62457dd1b9747bd8dfc9e8e35150f1536
Harsh Mehra <me.harshmehra07@gmail.com>
2024-02-24 21:52:04 +0530

    up

--- Creating-Server/server.js
+++ Creating-Server/server.js
@@ -3,7 +3,22 @@
 const express = require("express"); //-- Creating instance of express
 const app = express();
 
-//-- now running server on port number 3000
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+//-- Activating server on port number 3000
 app.listen(3000, () => {
   console.log("Server live at port num: 3000");
+});
+
+//-- Creating route
+app.get("/ex", (req, res) => {
+  res.send("Hello I am example of route");
+});
+
+app.post("/api/cars", (req, res) => {
+  const { name, brand } = req.body;
+  console.log(name);
+  console.log(brand);
+  res.send("Car submitted successfully");
 });
